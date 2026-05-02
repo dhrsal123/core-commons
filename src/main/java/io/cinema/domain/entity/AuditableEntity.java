b4417f87578cec649fd39e017e82125f3a21d2f6
Santiago Gonzalez Diaz <rstdtas@proton.me>
2026-05-02 14:21:25 -0400

    feat: add superbuilder to audit entity

--- src/main/java/io/cinema/domain/entity/AuditableEntity.java
+++ src/main/java/io/cinema/domain/entity/AuditableEntity.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuditableEntity {
